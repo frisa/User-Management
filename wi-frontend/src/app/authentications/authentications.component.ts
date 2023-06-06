14b346e909939c2f98631194d7e3778757fa7b3b
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2023-06-06 16:26:35 +0200

    Added directives

--- wi-frontend/src/app/authentications/authentications.component.ts
+++ wi-frontend/src/app/authentications/authentications.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { AuthRecord } from './authentication_record';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-authentications',
   templateUrl: './authentications.component.html',
   styleUrls: ['./authentications.component.css'],
-  standalone: true
+  standalone: true,
+  imports: [NgFor]
 })
 export class AuthenticationsComponent {
-  authentication: AuthRecord = {
-    id: 1, user: "Jan Fridrichovsky", authenticated: false,
-  };
+
+  authentications: AuthRecord[] = [
+    {id: 1, user: "Jan Fridrichovsky", authenticated: false},
+    {id: 2, user: "Nekdo Jinej", authenticated: false}
+  ];
 }
