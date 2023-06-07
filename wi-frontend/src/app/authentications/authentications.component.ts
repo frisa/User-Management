f39b85a9c0d51197b82917fa7e7a1a14665b4c30
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2023-06-07 14:33:20 +0200

    Some improvements in user interface

--- wi-frontend/src/app/authentications/authentications.component.ts
+++ wi-frontend/src/app/authentications/authentications.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { AuthRecord } from './authentication_record';
 import { NgFor } from '@angular/common';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-authentications',
   templateUrl: './authentications.component.html',
   styleUrls: ['./authentications.component.css'],
   standalone: true,
-  imports: [NgFor]
+  imports: [NgFor, UpperCasePipe]
 })
 export class AuthenticationsComponent {
 
   authentications: AuthRecord[] = [
     {id: 1, user: "Jan Fridrichovsky", authenticated: false},
-    {id: 2, user: "Nekdo Jinej", authenticated: false}
+    {id: 2, user: "Pepa Novak", authenticated: false},
+    {id: 2, user: "Deda Lebeda", authenticated: false}
   ];
 }
